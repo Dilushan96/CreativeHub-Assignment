f085ea2e2c13819acfbbb539bb184a9e8c0d4a12
Dilushan96 <dilushan.rajapakshe@gmail.com>
2023-02-01 00:41:16 +0530

    Create basic layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,12 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import img1 from '../assets/img1.png';
+import img2 from '../assets/img2.png';
+import img3 from '../assets/img3.png';
+import img5 from '../assets/img5.png';
+import img7 from '../assets/img7.png';
+import img9 from '../assets/img9.png';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,108 +20,132 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+      <main className="container-fluid">
+        <div className="row">
+          <div className="col">
+            Back
+          </div>
+          <div className="col">
+            <div className="row">
+              Digital Artist
+            </div>
+            <div className="row">
+              BANKSY_IS_DEAD
+            </div>
+          </div>
+          <div className="col">
+            Menu
           </div>
         </div>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
+        <div className="row">
+          <div className="col-md-2">
+            <div>
+              <Image src={img1} alt="testing" style={{ width: '100%', height: 'auto' }} />
+            </div>
+            <div>
+              Testing
+            </div>
+            <div>
+              22,600
+            </div>
+          </div>
+          <div className="col-md-2">
+            <div>
+              <Image src={img2} alt="testing" style={{ width: '100%', height: 'auto' }} />
+            </div>
+            <div class="row">
+              <div class="col">
+                Testing
+              </div>
+              <div class="col">
+                7,850
+              </div>
+            </div>
+          </div>
+          <div className="col-md-2">
+            <div>
+              <Image src={img3} alt="testing" style={{ width: '100%', height: 'auto' }} />
+            </div>
+            <div class="row">
+              <div class="col">
+                Testing
+              </div>
+              <div class="col">
+                10,330
+              </div>
+            </div>
+          </div>
+          <div className="col-md-2">
+            <div>
+              <Image src={img7} alt="testing" style={{ width: '100%', height: 'auto' }} />
+            </div>
+            <div class="row">
+              <div class="col">
+                Testing
+              </div>
+              <div class="col">
+                2,150
+              </div>
+            </div>
+          </div>
+          <div className="col-md-2">
+            <div>
+              <Image src={img5} alt="testing" style={{ width: '100%', height: 'auto' }} />
+            </div>
+            <div class="row">
+              <div class="col">
+                Testing
+              </div>
+              <div class="col">
+                2,150
+              </div>
+            </div>
           </div>
         </div>
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
+        <div className="row">
+          <div className="col">
+            <div className="row">CHILDREN OF</div>
+            <div className="row">FORGOTTEN</div>
+            <div className="row">GOD</div>
+          </div>
+          <div className="col">EXPLORE MORE ARTWORKS</div>
+          <div className="col">START BIDDING</div>
+        </div>
 
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
+        <div className="row justify-content-center text-center">
+          <div className="col">
+            Pellentesque...
+          </div>
+        </div>
 
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
+        <div className="row justify-content-center text-center">
+          <div className="col">
+            Eget quis...
+          </div>
+        </div>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+        <div className="row">
+          <div className="col">
+            <Image src={img1} alt="testing" style={{ width: '100%', height: 'auto' }} />
+          </div>
+          <div className="col">
+            <Image src={img1} alt="testing" style={{ width: '100%', height: 'auto' }} />
+          </div>
+          <div className="col">
+            <Image src={img1} alt="testing" style={{ width: '100%', height: 'auto' }} />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col">
+            <div className="row">Odio morbi</div>
+            <div className="row">Sed nunc nunc sagittis</div>
+          </div>
+          <div className="col">
+            <Image src={img9} alt="testing" style={{ width: '100%', height: 'auto' }} />
+          </div>
         </div>
       </main>
     </>
