fb3e28e6fbaa94dfeb822e8dbf79921c9379838b
Dilushan96 <dilushan.rajapakshe@gmail.com>
2023-02-04 01:06:52 +0530

    creating landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,9 @@ import img1 from '../assets/img1.png';
 import img2 from '../assets/img2.png';
 import img3 from '../assets/img3.png';
 import img5 from '../assets/img5.png';
+import img6 from '../assets/img6.png';
 import img7 from '../assets/img7.png';
+import img8 from '../assets/img8.png';
 import img9 from '../assets/img9.png';
 
 const inter = Inter({ subsets: ['latin'] })
@@ -20,46 +22,54 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="container-fluid">
-        <div className="row">
+      <main className="container mt-4">
+        <div className="row" style={{ fontWeight: 'bold' }}>
           <div className="col">
             Back
           </div>
           <div className="col">
             <div className="row">
-              Digital Artist
+              <div className="col text-center">
+                DIGITAL ARTIST
+              </div>
             </div>
             <div className="row">
-              BANKSY_IS_DEAD
+              <div className="col text-center">
+                <h3>BANKSY_IS_DEAD</h3>
+              </div>
             </div>
           </div>
           <div className="col">
-            Menu
+            <span className="float-end">Menu</span>
           </div>
         </div>
 
-        <div className="row">
+        <div className="row mt-5 justify-content-center" style={{ fontSize: '0.5rem' }}>
           <div className="col-md-2">
             <div>
               <Image src={img1} alt="testing" style={{ width: '100%', height: 'auto' }} />
             </div>
             <div>
-              Testing
+              Description description
             </div>
             <div>
-              22,600
+              <span className="price-tag">
+                22,600
+              </span>
             </div>
           </div>
-          <div className="col-md-2">
+          <div className="col-md-3">
             <div>
               <Image src={img2} alt="testing" style={{ width: '100%', height: 'auto' }} />
             </div>
-            <div class="row">
-              <div class="col">
-                Testing
+            <div className="row">
+              <div className="col">
+                Description
               </div>
-              <div class="col">
-                7,850
+              <div className="col">
+                <span className="float-end price-tag">
+                  7,850
+                </span>
               </div>
             </div>
           </div>
@@ -67,25 +77,29 @@ export default function Home() {
             <div>
               <Image src={img3} alt="testing" style={{ width: '100%', height: 'auto' }} />
             </div>
-            <div class="row">
-              <div class="col">
-                Testing
+            <div className="row">
+              <div className="col">
+                Description text text
               </div>
-              <div class="col">
-                10,330
+              <div className="col">
+                <span className="float-end price-tag">
+                  10,330
+                </span>
               </div>
             </div>
           </div>
-          <div className="col-md-2">
+          <div className="col-md-3">
             <div>
               <Image src={img7} alt="testing" style={{ width: '100%', height: 'auto' }} />
             </div>
-            <div class="row">
-              <div class="col">
-                Testing
+            <div className="row">
+              <div className="col">
+                Description
               </div>
-              <div class="col">
-                2,150
+              <div className="col">
+                <span className="float-end price-tag">
+                  2,150
+                </span>
               </div>
             </div>
           </div>
@@ -93,55 +107,100 @@ export default function Home() {
             <div>
               <Image src={img5} alt="testing" style={{ width: '100%', height: 'auto' }} />
             </div>
-            <div class="row">
-              <div class="col">
-                Testing
+            <div className="row">
+              <div className="col">
+                Description
               </div>
-              <div class="col">
-                2,150
+              <div className="col">
+                <span className="float-end price-tag">
+                  2,150
+                </span>
               </div>
             </div>
           </div>
         </div>
 
-        <div className="row">
-          <div className="col">
+        <div className="row mt-5 justify-content-end">
+          <div className="col-md-4">
             <div className="row">CHILDREN OF</div>
-            <div className="row">FORGOTTEN</div>
-            <div className="row">GOD</div>
+            <div className="row" style={{ fontWeight: 'bold', fontSize: '2rem' }}>FORGOTTEN</div>
+            <div className="row" style={{ fontWeight: 'bold', fontSize: '2rem' }}>GOD</div>
           </div>
-          <div className="col">EXPLORE MORE ARTWORKS</div>
-          <div className="col">START BIDDING</div>
+          <div className="col-md-3 align-self-center" style={{ fontWeight: 'bold', fontSize: '0.75rem' }}>EXPLORE MORE ARTWORKS</div>
+          <div className="col-md-3 align-self-end">
+            <span className="start-bidding-btn">
+              START BIDDING
+            </span>
+          </div>
         </div>
 
-        <div className="row justify-content-center text-center">
+        <div className="row justify-content-center text-center mt-5">
           <div className="col">
             Pellentesque...
           </div>
         </div>
 
         <div className="row justify-content-center text-center">
-          <div className="col">
-            Eget quis...
+          <div className="col-md-8 fs-1">
+            <span className="fw-bolder">Eget quis </span>
+            <span className="fw-light">ultrices id <br />eu nec </span>
+            <span className="fw-bolder">nulla. </span>
+            <span className="sometimes-btn">Sometimes</span>
           </div>
         </div>
 
-        <div className="row">
+        <div className="row mt-5">
           <div className="col">
-            <Image src={img1} alt="testing" style={{ width: '100%', height: 'auto' }} />
+            <Image src={img6} alt="testing" style={{ width: '100%', height: 'auto' }} />
+            <div className="fw-bold">Description line here</div>
+            <div>Subtitle line here</div>
           </div>
           <div className="col">
-            <Image src={img1} alt="testing" style={{ width: '100%', height: 'auto' }} />
+            <Image src={img7} alt="testing" style={{ width: '100%', height: 'auto' }} />
+            <div className="fw-bold">Description line here</div>
+            <div>Subtitle line here</div>
           </div>
           <div className="col">
-            <Image src={img1} alt="testing" style={{ width: '100%', height: 'auto' }} />
+            <Image src={img8} alt="testing" style={{ width: '100%', height: 'auto' }} />
+            <div className="fw-bold">Description line here</div>
+            <div>Subtitle line here</div>
           </div>
         </div>
 
-        <div className="row">
-          <div className="col">
-            <div className="row">Odio morbi</div>
-            <div className="row">Sed nunc nunc sagittis</div>
+        <div className="row my-5">
+          <div className="col fs-2" style={{ marginLeft: '5rem', marginRight: '2rem' }}>
+            <div className="row">
+              <div className="col">
+                Odio morbi
+              </div>
+            </div>
+            <div className="row fw-bolder text-end">
+              <div className="col">
+                Urna aliquam tinc
+              </div>
+            </div>
+            <div className="row fw-bolder">
+              <div className="col">
+                Aliquam pellentesque
+              </div>
+            </div>
+            <div className="row mt-5">
+              <div className="col fs-6">
+                <div style={{ marginLeft: '5rem' }}>
+                  Sed nunc nunc sagittis nulla verius.
+                  Sed nunc nunc sagittis nulla verius.
+                  Sed nunc nunc sagittis nulla verius.
+                  Sed nunc nunc sagittis nulla verius.
+                  Sed nunc nunc sagittis.
+                  Sed nunc nunc sagittis nulla verius.
+                  Sed nunc nunc sagittis nulla verius.
+                  Sed nunc nunc sagittis.
+                  Sed nunc nunc sagittis nulla verius.
+                  Sed nunc nunc sagittis nulla verius.
+                  Sed nunc nunc sagittis.
+                </div>
+              </div>
+            </div>
           </div>
           <div className="col">
             <Image src={img9} alt="testing" style={{ width: '100%', height: 'auto' }} />
